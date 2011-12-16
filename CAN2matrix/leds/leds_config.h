86db0064048d808b74cd87db88c4bf2e1b8020fe
Matthias Kleemann <M.Kleemann@gmx.net>
2011-12-16 22:01:27 +0100

    change documentation wthin file

--- CAN2matrix/leds/leds_config.h
+++ CAN2matrix/leds/leds_config.h
@@ -9,8 +9,9 @@
 #ifndef LEDS_CONFIG_H_
 #define LEDS_CONFIG_H_
 
-// define LEDs connected to your controller - name them as you wish, but keep
-// in mind that it matches the port/pin definitions below.
+/* @brief define LEDs connected to your controller - name them as you wish, but keep
+ *        in mind that it matches the port/pin definitions below.
+ */
 typedef enum
 {
    rxLED = 0,
@@ -18,15 +19,15 @@ typedef enum
    NUM_OF_LEDS = 2         // always the last one
 } eLED;
 
-// type of led control struct
+/* @brief type of led control struct
+ */
 typedef struct { pvuint8_t ddr;
                  pvuint8_t port;
                  uint8_t   pin;
                } ledType;
 
-// define global array to access port pins directly via port address
-// dim1: port address
-// dim2: pin
+/* @brief define global array to access port pins directly via port address
+ */
 static ledType ledPins[NUM_OF_LEDS] = { {&DDR(C), &PORT(C), 0},    // 0
                                         {&DDR(C), &PORT(C), 1}     // NUM_OF_LEDS - 1
                                       };

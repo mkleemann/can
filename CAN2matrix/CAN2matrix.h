522dbfa9574b3deda79ad248cc0136242420fe4e
Matthias Kleemann <M.Kleemann@gmx.net>
2011-12-27 19:40:59 +0100

    add ANY BEVERAGE-WARE license to files

--- CAN2matrix/CAN2matrix.h
+++ CAN2matrix/CAN2matrix.h
@@ -1,9 +1,21 @@
+/**
+ * ----------------------------------------------------------------------------
+ * "THE ANY BEVERAGE-WARE LICENSE" (Revision 42 - based on beer-ware license):
+ * <m.kleemann@gmx.net> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a be(ve)er(age) in return. (I don't
+ * like beer much.)
+ *
+ * Matthias Kleemann
+ * ----------------------------------------------------------------------------
+ **/
+
 /*
  * CAN2matrix.h
  *
  * Created: 28.11.2011 18:13:35
  *  Author: MKleemann
- */ 
+ */
 
 
 #ifndef CAN2MATRIX_H_
--- CAN2matrix/timer/timer_config.h
+++ CAN2matrix/timer/timer_config.h
@@ -1,9 +1,21 @@
+/**
+ * ----------------------------------------------------------------------------
+ * "THE ANY BEVERAGE-WARE LICENSE" (Revision 42 - based on beer-ware license):
+ * <m.kleemann@gmx.net> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a be(ve)er(age) in return. (I don't
+ * like beer much.)
+ *
+ * Matthias Kleemann
+ * ----------------------------------------------------------------------------
+ **/
+
 /*
  * timer_config.h
  *
  * Created: 28.11.2011 18:17:28
  *  Author: MKleemann
- */ 
+ */
 
 
 #ifndef TIMER_CONFIG_H_

5d906903f9817f16fed298624a5cb8879029b3ba
Matthias Kleemann <M.Kleemann@gmx.net>
2012-01-03 09:45:55 +0100

    first introduction of CAN ids; save intermediate stage

--- CAN2matrix/matrix.h
+++ CAN2matrix/matrix.h
@@ -26,24 +26,114 @@
 
 
 /***************************************************************************/
-/* Definition of Skoda CAN message ids                                     */
+/* Definition of car CAN message ids                                       */
 /***************************************************************************/
-#define CANID_1_IGNITION
-#define CANID_1_WHEEL_DATA
-#define CANID_1_REVERSE_GEAR
-#define CANID_1_LIGHT_STATUS
-//#define CANID_1_DIMMING
-#define CANID_1_PDC_STATUS
+// currently a mix of infotainment and convenience CAN bus signals
+// TODO: verify CAN signals in car
+
+// ignition by key status
+// another id could be 0x2C3 - verify
+#define CANID_1_IGNITION               0x271
+
+// wheel count values for navigation
+// also via gateway signal 0x351 (old!)
+#define CANID_1_WHEEL_DATA             0x455
+
+// gear box status via gateway signal
+#define CANID_1_REVERSE_GEAR           0x351
+
+// status of lights
+// to be verified
+#define CANID_1_LIGHT_STATUS           0x531
+
+// automatic and manual dimming
+#define CANID_1_DIMMING                0x635
+
+// also a gateway signal
+#define CANID_1_LANGUAGE_AND_UNIT      0x653
+
+// park distance control values
+// to be verified
+#define CANID_1_PDC_STATUS             0x54B
+
+// time and odometer
+// to be verified, but may also available via cluster
+#define CANID_1_TIME_AND_ODO           0x65D
+
+// vehicle identification number
+// to be verified
+#define CANID_1_VIN                    0x65F
+
+// steering wheel angle
+// to be verified
+#define CANID_1_STEERING_WHEEL         0x3C3
+
+// switch status, e.g. lights, horn, wipers, BC, ...
+// to be verified
+#define CANID_1_TURN_SIGNAL_CTRL       0x2C1
+
+// cruise control status
+// to be verified
+#define CANID_1_CRUISE_CONTROL         0x289
+
+// instrument cluster status, e.g. warning lights
+// to be verified
+#define CANID_1_COM_CLUSTER_STATUS1    0x621
+
+// instrument cluster status, e.g. date/time
+// to be verified
+#define CANID_1_COM_CLUSTER_STATUS2    0x623
+
+// multi function display status (sent by display!)
+// to be verified
+#define CANID_1_COM_DISPLAY_STATUS     0x62F
+
+// start signal for display communication
+// to be verified
+#define CANID_1_COM_DISP_START         0x2E8
+
+// communication channels
+// to be verified
+#define CANID_1_COM_RADIO_CLUSTER1     0x680
+#define CANID_1_COM_RADIO_CLUSTER2     0x681
+#define CANID_1_COM_NAVI_CLUSTER1      0x682
+#define CANID_1_COM_NAVI_CLUSTER2      0x683
+#define CANID_1_COM_PHONE_CLUSTER      0x685
+#define CANID_1_COM_COMPASS_CLUSTER    0x689
+
+// radio status signal, e.g. on/off, locked, ...
+// to be verified
+#define CANID_1_RADIO_STATUS           0x661
+
+// navigation status, e.g. wake up, bus sleep, ...
+// to be verified
+#define CANID_1_NAVI_STATUS            0x436
 
 /***************************************************************************/
-/* Definition of Chrysler CAN message ids                                  */
+/* Definition of radio CAN message ids                                     */
 /***************************************************************************/
-#define CANID_2_IGNITION               0x20B    // ignition and key status
-#define CANID_2_WHEEL_DATA             0x20E    // 4 wheels
-#define CANID_2_REVERSE_GEAR           0x21E    // rearview camera
-#define CANID_2_LIGHT_STATUS           0x309    // also dimming for unit keys
-#define CANID_2_DIMMING                0x194    // display
-#define CANID_2_METRIC_AND_LANG        0x2B0    // switches metric/languages
+
+// ignition and key status
+#define CANID_2_IGNITION               0x20B
+
+// wheel pulse counter
+#define CANID_2_WHEEL_DATA             0x20E
+
+// gear box status, e.g. for rear view camera and navigation
+#define CANID_2_REVERSE_GEAR           0x21E
+
+// light status and dimming for keys
+#define CANID_2_LIGHT_STATUS           0x309
+
+// dimming for main display
+#define CANID_2_DIMMING                0x194
+
+// switches units and language
+#define CANID_2_LANGUAGE_AND_UNIT      0x2B0
+
+/***************************************************************************/
+/* functions for matrix operations                                         */
+/***************************************************************************/
 
 /**
  * @brief fetch information from CAN1 and put to storage

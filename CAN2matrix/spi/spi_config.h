2a2f0ee1aa608f3fa62bae151b19984f0b5e02db
Matthias Kleemann <M.Kleemann@gmx.net>
2012-03-06 06:46:31 +0100

    change contact address in license information

--- CAN2matrix/spi/spi_config.h
+++ CAN2matrix/spi/spi_config.h
@@ -1,7 +1,7 @@
 /**
  * ----------------------------------------------------------------------------
  * "THE ANY BEVERAGE-WARE LICENSE" (Revision 42 - based on beer-ware license):
- * <m.kleemann@gmx.net> wrote this file. As long as you retain this notice you
+ * <dev@layer128.net> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a be(ve)er(age) in return. (I don't
  * like beer much.)
@@ -24,7 +24,7 @@
 /* @brief SPI_PRESCALER must have a value which is 2^n (n = 1..7)
  */
 #ifndef SPI_PRESCALER
-   #define SPI_PRESCALER      8
+   #define SPI_PRESCALER      128
 #endif
 
 
